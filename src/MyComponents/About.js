2fb1633f9883d44af8dac33170d8c07bd24096e2
Muhammad Arif <66298095+arfakl99@users.noreply.github.com>
2022-07-20 11:08:37 +0500

    my todolist react app

---
+++ src/MyComponents/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            
+            <h3>
+                <div className="container">
+                Hi this is me Arif.I am a web developer and designer. <br />
+                This is a simple reactjs based app where you can add <br />all those 
+                 tasks that you have to do daily.
+                And those <br />tasks wcich is completed you can easily deleted them.<br />
+                thank you
+                </div>
+
+                </h3>
+        </div>
+    )
+}
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export const Footer = () => { 
+    return (
+        <footer className="bg-primary text-white py-1">
+            <p className="text-center">
+            Copyright &copy; MyTodosList.com
+            </p>
+        </footer>
+    )
+}
